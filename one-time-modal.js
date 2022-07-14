7ceb84ff24a81ddaee71765bd2f929c67c059682
Toshihide Miyake <83240059+ToshihideMiyake@users.noreply.github.com>
2022-07-14 14:08:23 +0900

    Update one-time-modal.js

--- one-time-modal.js
+++ one-time-modal.js
@@ -1,3 +1,23 @@
+// You can change modal's style here
+const modalwrapper="position:fixed; top:0%; left:0%;"+
+                  "width:100vw;"+
+                  "height:100vh;"+
+                  "z-index:10;"+
+                  "display:none;"+
+                  "justify-content:center; align-items:center;"+
+                  "background-color:inherit;"
+
+const modalcss="display:flex;"+ 
+               "justify-content:center; align-items:center; flex-direction:column; gap:50px;"+ 
+               "font-size:1em;"+ 
+               "background-color:rgb(241, 245, 249);"+ 
+               "color:rgb(72, 72, 72);"+ 
+               "max-width:90vw;"+
+               "padding:50px;"+
+               "max-height:80vh;" +
+               "border-radius:20px;"
+
+
 function OneTimeModal(){
     const keycheck=localStorage.getItem("onetimemodal");
     if(keycheck==null){
@@ -21,28 +41,8 @@ function KeyReset(){ //For testing
     alert("Key has been reset.")
 }
 
-window.onload=()=>{
+document.addEventListener('DOMContentLoaded',()=>{
     document.querySelector(".OTmodal-wrapper").style=modalwrapper;
     document.querySelector(".onetimemodal").style=modalcss;
-}
-
-// You can change modal's style here
-const modalwrapper="position:fixed; top:0%; left:0%;"+
-                  "width:100vw;"+
-                  "height:100vh;"+
-                  "z-index:10;"+
-                  "display:none;"+
-                  "justify-content:center; align-items:center;"+
-                  "background-color:inherit;"
-
-const modalcss="display:flex;"+ 
-               "justify-content:center; align-items:center; flex-direction:column; gap:50px;"+ 
-               "font-size:1em;"+ 
-               "background-color:rgb(241, 245, 249);"+ 
-               "color:rgb(72, 72, 72);"+ 
-               "max-width:90vw;"+
-               "padding:50px;"+
-               "max-height:80vh;" +
-               "border-radius:20px;"
-              
-
+    OneTimeModal();
+})
